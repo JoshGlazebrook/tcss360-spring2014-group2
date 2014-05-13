d38df42e2ee0d2b7b8684e7d318e5644f945525c
m.messak <m.messak@140.142.218.112>
2014-05-13 16:57:14 -0700

    mina

--- group2/src/group2/Test.java
+++ group2/src/group2/Test.java
@@ -4,5 +4,5 @@ public class Test {
 	public int hello;
 	public String something = "sdfsdfd";
 	public String something2 = "lkjalfkja";
-	public String something3 = "Mina is the best";
+	public  String somedthing3 = "Mina is the best";
 }
