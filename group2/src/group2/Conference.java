a479a24a2e24670e4ecea65f12c3aa0fb39074cf
Jugbir <Jugbir@192.168.0.101>
2014-05-19 14:52:49 -0700

    Jay's conference class

--- group2/src/group2/Conference.java
+++ group2/src/group2/Conference.java
@@ -1,5 +1,37 @@
 package group2;
 
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The Conference class for the application
+ * @author Jugbir Singh - Jay
+ * @version 17 May, 2014
+ */
 public class Conference {
-	public String string = "Jay";
+	
+	ProgramChair programChair;
+	String date;
+	String deadline;
+	List<User> users;
+	
+	Conference(final User theStarter, final String theDate, final String theDeadline) {
+		//programChair = theStarter;
+		date = theDate;
+		deadline = theDeadline;
+		users = new LinkedList<User>();
+		users.add(theStarter);
+	}
+	
+	public void setPC(User starter) {
+		programChair = starter;
+	}
+	
+	public boolean checkExist(String data) {
+		
+	}
+	
+	checkIfReviewed(String title) {
+		
+	}
 }
