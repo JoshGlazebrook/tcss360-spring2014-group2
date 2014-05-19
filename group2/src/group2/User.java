7adf5571db5d4423a44739bf9ecbae221a024430
voyce24 <voyce24@gmail.com>
2014-05-19 11:35:59 -0700

    Anh

---
+++ group2/src/group2/User.java
@@ -0,0 +1,17 @@
+package group2;
+
+public class User {
+	private String id;
+	private String pass;
+	
+	public User(String the_id, String the_pass){
+		
+		id = the_id;
+		pass = the_pass;
+	}
+	
+	public void startConference(){
+		
+	}
+
+}
