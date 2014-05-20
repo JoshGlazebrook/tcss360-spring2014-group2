2fd3f36b8d502b0bb3c7c4aab2be019d4488423c
Jugbir <Jugbir@192.168.0.101>
2014-05-19 20:47:09 -0700

    Jay 5-19-14 8:64PM

--- group2/src/group2/Conference.java
+++ group2/src/group2/Conference.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * The Conference class for the application
+ * The Conference class for the system.
  * @author Jugbir Singh - Jay
  * @version 17 May, 2014
  */
@@ -28,13 +28,27 @@ public class Conference {
 		programChair = theStarter;
 	}*/
 	
-	public boolean checkExist(String data) {
+	public boolean checkPaperExists(String data) {
 		
 	}
 	
-	checkIfReviewed(String title) {
+	public boolean checkIfReviewed(String title) {
 		
 	}
 	
+	public User getProgramChair() {
+		return programChair;
+	}
 	
+	public String getDate() {
+		return date;
+	}
+	
+	public String getDeadline() {
+		return deadline;
+	}
+	
+	public List<User> getUsers() {
+		return users;
+	}
 }
