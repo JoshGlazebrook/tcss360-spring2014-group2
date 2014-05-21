86ad7f110d907c7162e077d83f0b432e2e8acd37
Jugbir <Jugbir@69.91.207.187>
2014-05-20 17:25:46 -0700

    abc

--- group2/src/group2/Conference.java
+++ group2/src/group2/Conference.java
@@ -13,29 +13,33 @@ public class Conference {
 	private final ProgramChair programChair;
 	private final String date;
 	private final String deadline;
+	private final String name;
 	private List<User> users;
-	private PaperManager papers;
+	private PaperManager paperManager;
+	//private List<Paper> papers;
 	
-	public Conference(final User theStarter, final String theDate, final String theDeadline) {
+	public Conference(final User theStarter, final String theName, final String theDate, final String theDeadline) {
 		programChair = (ProgramChair) theStarter;
+		name = theName;
 		date = theDate;
 		deadline = theDeadline;
 		users = new ArrayList<User>();
 		users.add(theStarter);
-		papers = new PaperManager();
+		papers = new ArrayList<Paper>();
+		paperManager = new PaperManager();
 	}
 	
 	/*
-	public void setPC(final User theStarter) {
-		programChair = theStarter;
-	}*/
-	
 	public boolean checkPaperExists(Paper paper) {
 		return papers.hasPaper(paper);
 	}
 	
 	public boolean checkIfReviewed(Paper paper) {
 		return paper.getReviewed();
+	}*/
+	
+	public void addPaper() {
+		
 	}
 	
 	public User getProgramChair() {
@@ -50,11 +54,13 @@ public class Conference {
 		return deadline;
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
 	public List<User> getUsers() {
 		return users;
 	}
 	
-	public List<Paper> getPapers() {
-		return papers;
-	}
+	
 }
