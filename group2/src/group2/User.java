e5b933d3020d0bdf6e06ae7acd5b0c6f07919674
Jugbir <Jugbir@205.175.113.57>
2014-05-27 11:04:58 -0700

    Changes

--- group2/src/group2/User.java
+++ group2/src/group2/User.java
@@ -1,20 +1,38 @@
 package group2;
 
 public class User {
+	
+	/**
+	 * The ID.
+	 */
 	final String id;
+	
+	/**
+	 * The password.
+	 */
 	String pass;
 	
-	public User(String the_id, String the_pass){
-		
+	/**
+	 * A constructor for User.
+	 * @param the_id, the id
+	 * @param the_pass, the password
+	 */
+	public User(String the_id, String the_pass) {
 		id = the_id;
 		pass = the_pass;
 	}
 	
+	/*
 	public void startConference(){
 		
+	}*/
+	
+	public String getID() { 
+		return id; 
 	}
 	
-	public String getID() { return id; }
-	public String getPass() { return pass; }
+	public String getPass() { 
+		return pass; 
+	}
 
 }
