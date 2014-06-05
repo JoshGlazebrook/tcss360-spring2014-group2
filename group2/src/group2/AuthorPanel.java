a6be392c2c1fcd9c3c062dec766b9d3c68ae9624
Jugbir <Jugbir@192.168.0.101>
2014-06-04 21:57:18 -0700

    Changed layout in AuthorPanel

--- group2/src/group2/AuthorPanel.java
+++ group2/src/group2/AuthorPanel.java
@@ -27,6 +27,7 @@ public class AuthorPanel extends JPanel {
 	private final JFileChooser fileChooser = new JFileChooser();
 	private final StringBuilder stringBuilder = new StringBuilder();
 	private JLabel lblConference = new JLabel("Conference Name");
+	private JLabel lblDate = new JLabel("Date");
 	private JLabel lblDeadline = new JLabel("Deadline");
 	private boolean submitted = false;
 
@@ -35,17 +36,33 @@ public class AuthorPanel extends JPanel {
 	 */
 	public AuthorPanel(final Conference curConf, final Author curAuthor) {
 		lblConference.setText(curConf.getName());
+		lblDate.setText(curConf.getDate());
 		lblDeadline.setText(curConf.getDeadline());
 		setLayout(null);
 		setSize(500, 500);
 		EditorKit kc;
 //		final JTextArea textArea = new JTextArea();
+//		final JTextField title = new JTextField();
+		
+		JLabel nameLabel = new JLabel("Name: ");
+		nameLabel.setBounds(15, 300, 84, 20);
+		add(nameLabel);
+
+		JLabel dateLabel = new JLabel("Date: ");
+		dateLabel.setBounds(15, 320, 84, 20);
+		add(dateLabel);
+		
+		JLabel deadlineLabel = new JLabel("Deadline: ");
+		deadlineLabel.setBounds(15, 340, 84, 20);
+		add(deadlineLabel);
+		
 		final JTextField title = new JTextField();
 		title.setBounds(15, 58, 480, 37);
 		add(title);
+
 		
 		final JEditorPane textArea = new JEditorPane();
-		textArea.setBounds(15, 95, 460, 203);
+		textArea.setBounds(36, 300, 460, 203);
 //		if (curConf.paperManager.hasPaper(curAuthor)) {
 //			textArea.setText(curConf.paperManager.getPaper(curAuthor).getData());
 //		}
@@ -67,11 +84,11 @@ public class AuthorPanel extends JPanel {
 				textArea.setText(stringBuilder.toString());
 			}
 		});
-		btnOpenFile.setBounds(358, 330, 127, 29);
+		btnOpenFile.setBounds(15, 370, 127, 29);
 		add(btnOpenFile);
 		
 		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.setBounds(370, 375, 115, 29);
+		btnSubmit.setBounds(180, 370, 115, 29);
 		btnSubmit.addActionListener(new ActionListener() {
 			
 			@Override
@@ -103,10 +120,10 @@ public class AuthorPanel extends JPanel {
 				        files[0]);	
 			}
 		});
-		btnRemoveFile.setBounds(15, 375, 133, 29);
+		btnRemoveFile.setBounds(333, 370, 133, 29);
 		add(btnRemoveFile);
 		
-	JButton btnSelectConfrence = new JButton("Select Confrence");
+//	JButton btnSelectConfrence = new JButton("Select Confrence");
 //	btnSelectConfrence.addActionListener(new ActionListener() {
 //			public void actionPerformed(ActionEvent e) {
 //				String[] conferences = { "Conference 1", "conference 2", "conference 3", "conference 4" };
@@ -122,17 +139,18 @@ public class AuthorPanel extends JPanel {
 //				changeLabel(lblDeadline, "10/12/2014");
 //			}
 //		});
-	btnSelectConfrence.setBounds(15, 330, 149, 29);
-	add(btnSelectConfrence);
+//	btnSelectConfrence.setBounds(15, 450, 149, 29);
+//	add(btnSelectConfrence);
 		
-		
-		lblConference.setBounds(getWidth()/2, 330, 127, 20);
+		lblConference.setBounds(70, 300, 127, 20);
 		add(lblConference);
 		
-		lblDeadline.setBounds(getWidth()/2, 379, 83, 20);
+		lblDate.setBounds(70, 320, 83, 20);
+		add(lblDate);
+		
+		lblDeadline.setBounds(70, 340, 83, 20);
 		add(lblDeadline);
-		
-		
+
 		JLabel lblAuthor = new JLabel("Author");
 		lblAuthor.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblAuthor.setBounds(205, 15, 89, 37);
@@ -146,11 +164,8 @@ public class AuthorPanel extends JPanel {
 	
 	public void getFile() throws Exception{
 		if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
-			
 			java.io.File file = fileChooser.getSelectedFile();
-			
 			Scanner input = new Scanner(file);
-			
 			while(input.hasNext()){
 				stringBuilder.append(input.nextLine());
 				stringBuilder.append("\n");
