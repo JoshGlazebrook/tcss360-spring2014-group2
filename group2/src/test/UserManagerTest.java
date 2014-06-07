8dbd2c26e8f2258b272ae03cac1de62ed7fdfbf9
JAMIE <voyce24@gmail.com>
2014-06-06 20:16:00 -0700

    user manager test

--- group2/src/test/UserManagerTest.java
+++ group2/src/test/UserManagerTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import static org.junit.Assert.*;
-import group2.User;
 import group2.UserManager;
 
 import java.util.HashMap;
@@ -10,23 +9,26 @@ import org.junit.Test;
 
 public class UserManagerTest {
 
-	//@Test
-	/*public void testFindID() {
+	@Test
+	public void testFindID() {
 		UserManager now = new UserManager();
-		User cur = new User("myuser", "jin");
-		HashMap<String, User> findTable = new HashMap<String, User>();
-		findTable.put(cur.getUserName(), cur);
+		HashMap<String, String> findTable = new HashMap<String, String>();
+		findTable.put("myuser", "jin");
 		now.allUsers = findTable;
-		assertEquals("same pass", now.findID("myuser").getPass(), "jin");
-		assertNull("Null", now.findID("mako"));
+		assertTrue("Got the user",now.findID("myuser", "jin"));
+		assertFalse("Wrong username", now.findID("id", "jin"));
+		assertFalse("Wrong password", now.findID("myuser", "pass"));
 	}
 
-	//@Test
+	@Test
 	public void testSignUp() {
 		UserManager now = new UserManager();
-		HashMap<String, User> signTable = new HashMap<String, User>();
-		User cur = new User("mysignup", "jung");
+		HashMap<String, String> signTable = new HashMap<String, String>();
+		signTable.put("myuser", "jin");
 		now.allUsers = signTable;
-		
-	}*/
+		assertFalse("Existed username", now.signUp("myuser", "jung"));
+		assertFalse("Empty username", now.signUp("", "jin"));
+		assertFalse("Empty password", now.signUp("myId", ""));
+		assertTrue("Success", now.signUp("myID", "jin"));
+	}
 }
